b474838f9a54fb8fc2a093357305390eee381777
Veena-S <veenasivan@gmail.com>
2021-01-14 06:06:00 +0800

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './styles.scss';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log('hello');
+function BigAnnouncement() {
+  const myEl = (
+    <div>
+      <h1 className="hero-text">
+        Heyyyy <span className="warning">Wow!</span>
+      </h1>
+      <p>Lorem Ipsum!!</p>
+    </div>
+  );
+
+  console.log('myEl:', myEl);
+  return myEl;
+}
+
+// create an element that React will render stuff into
+const rootElement = document.createElement('div');
+
+// put that element onto the page
+document.body.appendChild(rootElement);
+
+const myContainer = (
+  <div>
+    <p>1. <BigAnnouncement /></p>
+    <BigAnnouncement />
+    <BigAnnouncement />
+    <BigAnnouncement />
+  </div>
+);
+
+// have react render the JSX element into the root element.
+render(myContainer, rootElement);
--- webpack_conf/webpack.common.js
+++ webpack_conf/webpack.common.js
@@ -2,9 +2,9 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
+  // entry: {
+  //   app: './src/index.js',
+  // },
   plugins: [
     new MiniCssExtractPlugin(),
   ],
